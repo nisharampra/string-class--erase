53909614d3d36789eb617b6c0f8c02ae419ee3ac
Nisha Ramprasath <115043583+nisharampra@users.noreply.github.com>
2024-01-08 23:06:16 +0800

    Add files via upload

---
+++ string class -erase/string class -erase/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  string class -erase
+//
+//  Created by Nisha Ramprasath on 3/11/23.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    string str="programing";
+    str.erase();
+    cout<<str<<endl;
+    return 0;
+}
